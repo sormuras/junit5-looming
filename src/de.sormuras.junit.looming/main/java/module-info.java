4e89587dc5a0af7ef1d8999c79ef073a43a7d2b2
Christian Stein <sormuras@gmail.com>
2019-08-13 22:37:39 +0200

    Use fully-qualified class name in module declaration

--- src/de.sormuras.junit.looming/main/java/module-info.java
+++ src/de.sormuras.junit.looming/main/java/module-info.java
@@ -1,9 +1,7 @@
-import de.sormuras.junit.looming.LoomTestEngine;
-
 module de.sormuras.junit.looming {
   requires org.junit.platform.engine;
   requires org.junit.platform.console;
 
   provides org.junit.platform.engine.TestEngine with
-      LoomTestEngine;
+      de.sormuras.junit.looming.LoomTestEngine;
 }
