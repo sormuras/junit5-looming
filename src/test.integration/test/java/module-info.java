9e492ac3b034eee4cd51d2b9631ae90364987d14
Christian Stein <sormuras@gmail.com>
2020-01-25 20:31:23 +0100

    Add missing requires directive

--- src/test.integration/test/java/module-info.java
+++ src/test.integration/test/java/module-info.java
@@ -3,5 +3,6 @@ open /*test*/ module test.integration {
   requires de.sormuras.junit.looming;
   // module we're testing with
   requires test.base;
+  requires org.junit.platform.engine;
   uses org.junit.platform.engine.TestEngine;
 }
