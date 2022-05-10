46913a945eee191074efb405e025b8471caa7ec9
Christian Stein <sormuras@gmail.com>
2022-05-10 15:53:14 +0200

    Configure project correctly

--- .bach/src/build.java
+++ .bach/src/build.java
@@ -13,25 +13,19 @@ class build {
         new Bach(
             Configuration.ofDefaults(),
             Project.ofDefaults()
+                .withModule("main", "com.github.sormuras.junit.looming")
+                .withTargetsJava("main", Runtime.version().feature())
+                .withAdditionalCompileJavacArguments("main", "--enable-preview")
                 .withExternalModules("junit", "5.8.2")
                 .withRequiresModule("org.junit.jupiter")
                 .withRequiresModule("org.junit.platform.console"));
 
     bach.run("cache"); // go offline by caching all required external assets that are missing
-
-    bach.run(
-        ToolCall.of("javac")
-            .with("--release", 19)
-            .with("--enable-preview")
-            .with("--module", "com.github.sormuras.junit.looming")
-            .with("--module-source-path", ".")
-            .with("--module-path", ".bach/external-modules")
-            .with("-d", ".bach/out/main/classes/java-19-preview"));
-
+    bach.run("compile"); // compile all modules in all project spaces
     bach.run(
         ToolFinder.of(
             ModuleFinder.of(
-                Path.of(".bach/out/main/classes/java-19-preview"),
+                Path.of(".bach/out/main/modules"),
                 Path.of(".bach/external-modules")),
             true,
             "com.github.sormuras.junit.looming"),
